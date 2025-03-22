2e623fc71317c08b3089de05cc71df718314f520
Xin <zzx2002917@gmail.com>
2025-03-23 00:44:44 +0100

    update profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -16,5 +16,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             'id': user.id,
             'full_name': user.full_name,
             'email': user.email,
+            'role': user.role,
         }
         return representation
