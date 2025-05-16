60ae0e7b549dea7b89525ad8c3da7fa5dd176501
XIN917 <zzx2002917@gmail.com>
2025-05-16 18:11:20 +0200

    Fix

--- tfms/storage.py
+++ tfms/storage.py
@@ -2,22 +2,25 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
+
 class StaticS3Boto3Storage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = getattr(settings, "STATICFILES_LOCATION", "")
     default_acl = 'public-read'
 
     def __init__(self, *args, **kwargs):
-        if settings.MINIO_ACCESS_URL:
+        minio_url = getattr(settings, "MINIO_ACCESS_URL", None)
+        if minio_url:
             self.secure_urls = False
-            self.custom_domain = settings.MINIO_ACCESS_URL
+            self.custom_domain = minio_url
         super().__init__(*args, **kwargs)
+
 
 class S3MediaStorage(S3Boto3Storage):
     default_acl = 'public-read'
 
     def __init__(self, *args, **kwargs):
-        if settings.MINIO_ACCESS_URL:
+        minio_url = getattr(settings, "MINIO_ACCESS_URL", None)
+        if minio_url:
             self.secure_urls = False
-            self.custom_domain = settings.MINIO_ACCESS_URL
+            self.custom_domain = minio_url
         super().__init__(*args, **kwargs)
-
