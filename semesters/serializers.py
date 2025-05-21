1ee35c07730aeb90de2549e0c21855b583a30548
XIN917 <zzx2002917@gmail.com>
2025-05-21 18:59:41 +0200

    Fix

--- semesters/serializers.py
+++ semesters/serializers.py
@@ -5,3 +5,12 @@ class SemesterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Semester
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Format time fields as HH:MM
+        for field in ['daily_start_time', 'daily_end_time', 'pre_duration']:
+            value = data.get(field)
+            if value:
+                data[field] = value[:5]  # Truncate to HH:MM
+        return data
